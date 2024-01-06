f1fef22d5dfa4c3d1b979577d08c9a8f3f7d6bc4
Rabiul Hossen <rhsimon69@gmail.com>
2024-01-06 20:24:14 +0600

    style changer project done

---
+++ Image_Style_Changer/script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.box input');
+
+inputs.forEach((inputs) => inputs.addEventListener('change', update));
+
+function update() {
+     const suffixData = this.dataset.sizing || '';
+
+     document.documentElement.style.setProperty(`--${this.name}`,
+          this.value + suffixData);
+}
